9406e3fc0af37e082fa897621adde6c6adffee50
Lucio Paiva <luciopaiva@gmail.com>
2022-03-05 21:58:28 +0000

    v1.0.0

--- main.lua
+++ main.lua
@@ -1,8 +1,16 @@
 
 dofile("file-sequence.lua")
 
+debugMode = false
+
 unsavedByFilename = {}
 spriteByFilename = {}
+
+function dbg(msg)
+    if debugMode then
+        print(msg)
+    end
+end
 
 function isSaved(sprite)
     return app.fs.isFile(sprite.filename)
@@ -11,9 +19,9 @@ end
 function onSpriteChange(filename)
     local obj = spriteByFilename[filename]
     obj.changes = obj.changes + 1
-    print("Sprite " .. filename .. " has changed " .. obj.changes .. " times")
+    dbg("Sprite " .. filename .. " has changed " .. obj.changes .. " times")
     local filename = getTimeLapseFileName(filename, nil)
-    print("Will save snapshot " .. filename)
+    dbg("Will save snapshot " .. filename)
     if filename then
         obj.sprite:saveCopyAs(filename)
     end
@@ -29,17 +37,17 @@ function registerSprite(sprite)
 end
 
 function promoteUnsavedFile(sprite, filename, listener)
-    print("Detected a file name change! Sprite " .. filename .. " is now " .. app.activeSprite.filename)
+    dbg("Detected a file name change! Sprite " .. filename .. " is now " .. app.activeSprite.filename)
     unsavedByFilename[filename] = nil
     sprite.events:off(listener)
-    print("Unregistered filename change listener for " .. filename)
+    dbg("Unregistered filename change listener for " .. filename)
     handleSavedSprite(app.activeSprite)
 end
 
 function handleUnsavedSprite(sprite)
     local filename = sprite.filename  -- this will be something like "Sprite-001" in an unsaved file
     if unsavedByFilename[filename] == nil then
-        print("Registering unsaved sprite " .. filename)
+        dbg("Registering unsaved sprite " .. filename)
         function callback()
             promoteUnsavedFile(sprite, filename, callback)
         end
@@ -51,7 +59,7 @@ end
 function handleSavedSprite(sprite)
     local filename = sprite.filename
     if spriteByFilename[filename] == nil then
-        print("Registering saved sprite " .. filename)
+        dbg("Registering saved sprite " .. filename)
         registerSprite(sprite)
         -- call it once straight away
         onSpriteChange(filename)
@@ -72,26 +80,15 @@ function onSiteChange()
 end
 
 function init(plugin)
-    print("Aseprite is initializing timelapse plugin")
+    dbg("Aseprite is initializing timelapse plugin")
 
     app.events:on("sitechange", onSiteChange)
 
     if plugin.preferences.count == nil then
         plugin.preferences.count = 0
     end
-
-  --
---   plugin:newCommand{
---     id="Timelapse",
---     title="Timelapse",
---     group="main_menu",
---     onclick=function()
---       plugin.preferences.count = plugin.preferences.count+1
---       print("Menu clicked")
---     end
---   }
 end
 
 function exit(plugin)
-    print("Aseprite is closing timelapse plugin...")
+    dbg("Aseprite is closing timelapse plugin...")
 end
